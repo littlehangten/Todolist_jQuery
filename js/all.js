bb3f60a538999494af19e77f1b41ad8e676d36df
Daniel <littlehangten@gmail.com>
2019-10-26 21:07:18 +0800

    add update feature for doto-list

--- js/all.js
+++ js/all.js
@@ -3,21 +3,32 @@ $(document).ready(function () {
     var source = $('#todo-list-item-template').html()
     var todoTemplate = Handlebars.compile(source)
 
-    // create  todo list
-    $('li.new').find('.content').blur(function(event) {
-        var todo = $(event.currentTarget).text()
-        todo = todo.trim()
+    // enter editor mode
+    $('#todo-list')
+        .on('dblclick', '.content', function(event) {
+            $(this).prop('contenteditable', true).focus()
+        })
+        .on('blur', '.content', function(event) {
+            var isNew = $(this).closest('li').is('.new')
+            if (isNew) {
+                // create todo list
+                var todo = $(event.currentTarget).text()
+                todo = todo.trim()
 
-        if(todo.length > 0 ) {
-            todo = {
-                is_complete: false,
-                content: todo,
+                if (todo.length > 0) {
+                todo = {
+                    is_complete: false,
+                    content: todo,
+                }
+                var li = todoTemplate(todo);
+                $(event.currentTarget).closest('li').before(li)
+                }
+                // clear new todo list item
+                $(event.currentTarget).empty()
+            }else {
+                // update todo list
+                $(this).prop('contenteditable', false)
             }
-            var li = todoTemplate(todo);
-            $(event.currentTarget).closest('li').before(li)
-        }
-        // clear new todo list item
-        $(event.currentTarget).empty()
-    })
+        })
 
 });
