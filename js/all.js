54362bc3b246960ba337cc451006e6888cf544ac
Daniel <littlehangten@gmail.com>
2019-10-26 19:32:11 +0800

    finish create for todo-list-item

--- js/all.js
+++ js/all.js
@@ -1,13 +1,21 @@
-// create  todo list
 $(document).ready(function () {
 
+    var source = $('#todo-list-item-template').html()
+    var todoTemplate = Handlebars.compile(source)
+
+    // create  todo list
     $('li.new').find('.content').blur(function(event) {
         var todo = $(event.currentTarget).text()
-        // 需要一個完整 todo list item 的架構
-        var li = $('.template').find('li').clone()
-        li.find('.content').text(todo)
-        $(event.currentTarget).closest('li').before(li)
+        todo = todo.trim()
 
+        if(todo.length > 0 ) {
+            todo = {
+                is_complete: false,
+                content: todo,
+            }
+            var li = todoTemplate(todo);
+            $(event.currentTarget).closest('li').before(li)
+        }
         // clear new todo list item
         $(event.currentTarget).empty()
     })
